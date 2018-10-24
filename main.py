3d745c1eec781415354ee7945755b81101703cc4
xwp <xwp_fullstack@163.com>
2018-10-24 16:38:54 +0800

    修改index视图函数

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ class Config:
 app.config.from_object(Config)
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    pass
+    return 'index'
 if __name__ == '__main__':
     app.run(debug=True)
